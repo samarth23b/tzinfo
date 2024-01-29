1a3e6b4138c6e84711ed8010ca9c376ed44f23ed
samarth23b <samarth.benawat@amagi.com>
2024-01-29 15:14:39 +0530

    add configs in ENV variables

--- lib/tzinfo/amagi/defamagi/Amagi.rb
+++ lib/tzinfo/amagi/defamagi/Amagi.rb
@@ -10,8 +10,8 @@ module TZInfo
         include TimezoneDefinition
         d = Date.current
         d = Date.parse(ENV["AMAGI_DST_DATE"]) if ENV["AMAGI_DST_DATE"]
-        switch_over_epoch = Rails.configuration.respond_to?(:amagi_dst_switch_time) ? Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + Rails.configuration.amagi_dst_switch_time ).to_i : Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " 08:30:00").to_i
-        adst_offset = Rails.configuration.respond_to?(:amagi_dst_switch_offset) ? Rails.configuration.amagi_dst_switch_offset : 23400
+        switch_over_epoch = ENV["AMAGI_DST_SWITCH_TIME"] ? Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") +" " + ENV["AMAGI_DST_SWITCH_TIME"] ).to_i : Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " " + "08:30:00").to_i
+        adst_offset = ENV["AMAGI_DST_SWITCH_OFFSET"] ? ENV["AMAGI_DST_SWITCH_OFFSET"] : 23400
         timezone 'Amagi' do |tz|
           tz.offset :o0, 19800, 0, :AST
           tz.offset :o1, adst_offset, 0, :ADST

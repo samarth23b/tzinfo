c4fec0016dcd6e28e3a5dce39c176da9a8eb5d34
samarth23b <samarth.benawat@amagi.com>
2024-01-30 17:40:36 +0530

    change adst_offset to integer

--- lib/tzinfo/amagi/defamagi/Amagi.rb
+++ lib/tzinfo/amagi/defamagi/Amagi.rb
@@ -11,7 +11,7 @@ module TZInfo
         d = Date.current
         d = Date.parse(ENV["AMAGI_DST_DATE"]) if ENV["AMAGI_DST_DATE"]
         switch_over_epoch = ENV["AMAGI_DST_SWITCH_TIME"] ? Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") +" " + ENV["AMAGI_DST_SWITCH_TIME"] ).to_i : Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " " + "08:30:00").to_i
-        adst_offset = ENV["AMAGI_DST_SWITCH_OFFSET"] ? ENV["AMAGI_DST_SWITCH_OFFSET"] : 23400
+        adst_offset = ENV["AMAGI_DST_SWITCH_OFFSET"] ? (ENV["AMAGI_DST_SWITCH_OFFSET"]).to_i : 23400
         timezone 'Amagi' do |tz|
           tz.offset :o0, 19800, 0, :AST
           tz.offset :o1, adst_offset, 0, :ADST
